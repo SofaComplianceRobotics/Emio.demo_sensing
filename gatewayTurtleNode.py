bea074ba545af56313425b1f6cecdfb9c4a4a603
EulalieCoevoet <eulalie.coevoet@gmail.com>
2025-04-09 18:53:24 +0200

    add gateway

---
+++ gatewayTurtleNode.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+from math import atan2, sqrt
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32MultiArray
+from geometry_msgs.msg import Twist, Vector3
+from turtlesim.msg import Pose
+
+
+class GatewayTurtleNode(Node):
+
+    def __init__(self, publishing_topic='/turtle1/cmd_vel', subscribed_topic='/Sensor/Force'):
+        super().__init__('gateway_node')
+        self.publish_topic = publishing_topic
+        self.subscribe_topic = subscribed_topic
+        self.publisher = self.create_publisher(Twist, self.publish_topic, 10)
+        self.last_theta = 0.0
+
+        self.subscription = self.create_subscription(
+            Float32MultiArray,
+            self.subscribe_topic,
+            self.listener_callback,
+            10)
+        
+        self.subscriptionPose = self.create_subscription(
+            Pose,
+            '/turtle1/pose',
+            self.pose_callback,
+            10)
+        self.get_logger().info(f'[GatewayNode] GatewayNode created with publisher on {self.publish_topic} and subscriber on {self.subscribe_topic}')
+    
+    def pose_callback(self, msg):
+        self.last_theta = msg.theta
+
+    def send_move_msg(self, linear_x, linear_y, linear_z, angular_x, angular_y, angular_z ):
+        msg = Twist()
+        msg.linear = Vector3(x=linear_x, y=linear_y, z=linear_z)
+        msg.angular = Vector3(x=angular_x, y=angular_y, z=angular_z)
+        self.publisher.publish(msg)
+        self.get_logger().info(f'[GatewayNode] send_move - Publishing: {msg.linear} {msg.angular} to {self.publish_topic}')
+
+    def listener_callback(self, msg):
+        # Log the received message
+        self.get_logger().info(f'[GatewayNode] Received: {msg}')
+
+        # Convert Float32MultiArray to Twist
+        # Assuming msg.data is a list of 3 floats
+        if len(msg.data) != 3:
+            self.get_logger().error(f'[GatewayNode] Invalid message length: {len(msg.data)}')
+            return
+        twist_msg = Twist() 
+
+        # convert the cartesian coordinates to polar coordinates
+        cartesian = msg.data
+        polar = [sqrt(cartesian[0]**2 + cartesian[2]**2)*1e-1, atan2(cartesian[2], -cartesian[0]), 0]
+
+        self.get_logger().info(f'[GatewayNode] Polar coordinates: {polar}')
+
+        # Get the last polar coabordinates of turtlebot and compute angle difference with the new polar coordinates
+        polar[1] = polar[1] - self.last_theta
+
+        self.get_logger().info(f'[GatewayNode] Last Theta {self.last_theta}')
+
+        # Set the linear and angular velocities
+        twist_msg.linear.x = polar[0]
+        twist_msg.angular.z = polar[1]
+
+        # Forward the message to the publisher
+        self.send_move_msg(
+            twist_msg.linear.x,
+            twist_msg.linear.y,
+            twist_msg.linear.z,
+            twist_msg.angular.x,
+            twist_msg.angular.y,
+            twist_msg.angular.z
+        )
+        self.get_logger().info(f'[GatewayNode] Forwarded: {twist_msg.linear} {twist_msg.angular}')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    gateway = GatewayTurtleNode()
+    rclpy.spin(gateway)
+    gateway.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
