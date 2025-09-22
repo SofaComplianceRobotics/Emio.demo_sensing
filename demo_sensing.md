9b8ec0d0f026bb96fd10ed9da5ed8397626cf0f7
EulalieCoevoet <eulalie.coevoet@gmail.com>
2025-09-22 18:43:01 +0200

    wip: adds teleoperation demo

--- demo_sensing.md
+++ demo_sensing.md
@@ -43,4 +43,19 @@ python gatewayTurtleNode.py
 
 On the GUI, select the window __Windows>Input/Output__. From this window __Input/Output__, in the output section, select the topic __Sensor/Force__ and click toggle the __Publish__ button.
 
+### Teleoperation Demo
+
+We can use Emio as a joystick to control a second Emio robot.
+
+In a terminal, from the demo_sensing directory:
+```command
+source /opt/ros/jazzy/setup.sh
+pyenv local 3.12
+python gatewayTCPTargetEmioGripper.py
+```
+
+Run a second simulation by clicking the button below, then press the play button to start the simulation (top middle of the 3D view). Set up the second robot Emio like in the simulation, [connect the robot to your computer](https://docs-support.compliance-robotics.com/docs/next/Users/Emio/getting-started-with-emio/#connecting-emio-to-your-computer) and then [connect the simulation](https://docs-support.compliance-robotics.com/docs/next/Users/SOFARobotics/GUI-user-manual/#controlling-the-real-robot). You can now play with the demo. Move the effector of the first real robot and observe the motion of the second robot.
+
+#runsofa-button("assets/labs/demo_sensing/demo_teleoperation.py")
+
 ::::
---
+++ demo_teleoperation.py
@@ -0,0 +1,72 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../../")
+
+import os.path
+import Sofa
+
+
+def createScene(rootnode):
+    from utils.header import addHeader, addSolvers
+    from parts.gripper import Gripper
+    from parts.controllers.assemblycontroller import AssemblyController
+    import Sofa.ImGui as MyGui
+    from parts.emio import Emio
+    from parts.controllers.trackercontroller import DotTracker
+
+    settings, modelling, simulation = addHeader(rootnode, inverse=True)
+    addSolvers(simulation)
+
+    rootnode.dt = 0.03
+    rootnode.gravity = [0., -9810., 0.]
+    rootnode.VisualStyle.displayFlags.value = ["hideBehavior", "hideWireframe"]
+
+    # Add Emio to the scene
+    emio = Emio(name="Emio",
+                legsName=["longleg"],
+                legsModel=["beam"],
+                legsPositionOnMotor=["counterclockwisedown", "clockwisedown", "counterclockwisedown", "clockwisedown"],
+                centerPartName="whitepart",
+                centerPartType="deformable",
+                centerPartModel="beam",
+                centerPartClass=Gripper,
+                extended=True)
+    if not emio.isValid():
+        return
+
+    simulation.addChild(emio)
+    emio.attachCenterPartToLegs()
+    emio.addObject(AssemblyController(emio))
+
+    # Add effector
+    emio.effector.addObject("MechanicalObject", template="Rigid3", position=[0, 0, 0, 0, 0, 0, 1] * 4)
+    emio.effector.addObject("RigidMapping", rigidIndexPerPoint=[0, 1, 2, 3])
+
+    # Target
+    effectorTarget = modelling.addChild('Target')
+    effectorTarget.addObject('EulerImplicitSolver', firstOrder=True)
+    effectorTarget.addObject('CGLinearSolver', iterations=50, tolerance=1e-10, threshold=1e-10)
+    effectorTarget.addObject('MechanicalObject', template='Rigid3',
+                             position=[0, -170, 0, 0, 0, 0, 1],
+                             showObject=True, showObjectScale=20)
+
+    # Add inverse components and GUI
+    emio.addInverseComponentAndGUI(effectorTarget.getMechanicalState().position.linkpath, barycentric=True)
+    MyGui.MoveWindow.setTCPLimits(-100, 100,
+                                    emio.motors[0].JointActuator.minAngle.value,
+                                    emio.motors[0].JointActuator.maxAngle.value)
+
+    TCP = modelling.addChild("TCP")
+    TCP.addObject("MechanicalObject", template="Rigid3", position=emio.effector.EffectorCoord.barycenter.linkpath)
+    MyGui.setIPController(rootnode.Modelling.Target, TCP, rootnode.ConstraintSolver)
+
+    # GUI slider for the gripper opening
+    MyGui.MoveWindow.addAccessory("Gripper's opening (mm)", emio.centerpart.Effector.Distance.DistanceMapping.restLengths, 8, 70)
+    MyGui.ProgramWindow.addGripper(emio.centerpart.Effector.Distance.DistanceMapping.restLengths, 8, 70)
+    MyGui.IOWindow.addSubscribableData("/Gripper", emio.centerpart.Effector.Distance.DistanceMapping.restLengths)
+
+    # Components for the connection to the real robot 
+    emio.addConnectionComponents()
+        
+    return rootnode
